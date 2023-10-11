cc6b4e44902cb7692f7c35da418bceb468e99cb7
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-11 19:05:02 +0530

    update c-cpp/armstrong_num.cpp

--- c-cpp/armstrong_num.cpp
+++ c-cpp/armstrong_num.cpp
@@ -1,34 +1,14 @@
 //wap to enter a number and print it is bahubali or not
 #include <stdio.h>
-
-int pow(int v, int l){
-	int pwr=1;
-	for(int i=1; i<=l; i++){
-    pwr *= v;
-  }
-	return pwr;
-}
-
+int pow(int v, int l){int pwr=1;
+	for(int i=1; i<=l; i++){pwr *= v; }
+	return pwr;}
 int main(){
 	int x, s=0;
-	while(s>=0){	
-		printf("Enter a number : ");
-		scanf("%d", &x);
-		int c = x, n=x, l=0;
-		while(n>0){
-      l++; 
-      n /=10;
-    }
-		for(int i=1; i<=l; i++){
-			int mod = x%10; 
-			s += pow(mod, l); x/=10;}
-		if(c==s){
-      printf("%d Armstrong\n", c);
-    }
-		else{
-      printf("%d Not Armstrong\n", c);
-    }
-    s = 0;
-	}
-	return 87;
+	while(s>=0){printf("Enter a number : ");scanf("%d", &x);int c = x, n=x, l=0;
+		while(n>0){l++; n /=10;}
+		for(int i=1; i<=l; i++){int mod = x%10; s += pow(mod, l); x/=10;}
+		if(c==s){printf("%d Armstrong\n", c);}
+		else{printf("%d Not Armstrong\n", c); }s = 0;
+	}return 87;
 }
