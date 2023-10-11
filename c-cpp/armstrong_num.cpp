16466e36490a2821f75a392635f7eb8e096ae397
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-11 16:44:24 +0530

    Create armstrong_num.cpp

---
+++ c-cpp/armstrong_num.cpp
@@ -0,0 +1,35 @@
+//wap to enter a number and print it is bahubali or not
+#include <stdio.h>
+
+int pval(int v, int l){
+	int pwr=1;
+	for(int i=1; i<=l; i++){
+    pwr *= v;
+  }
+	return pwr;
+}
+
+int main(){
+	int x, s=0;
+	while(s>=0){	
+		printf("Enter a number : ");
+		scanf("%d", &x);
+		int c = x, n=x, l=0;
+		while(n>0){
+      l++; 
+      n /=10;
+    }
+		for(int i=1; i<=l; i++){
+			int mod = x%10; 
+      power = pval(mod, l);
+			s += power; x/=10;}
+		if(c==s){
+      printf("%d Armstrong\n", c);
+    }
+		else{
+      printf("%d Not Armstrong\n", c);
+    }
+    s = 0;
+	}
+	return 87;
+}
