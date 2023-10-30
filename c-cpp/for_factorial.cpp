185370ac88d1e484f581a89eb82565c156fdc2e6
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-10-30 23:31:49 +0530

    Create for_factorial.cpp

---
+++ c-cpp/for_factorial.cpp
@@ -0,0 +1,10 @@
+//wap to print the factorial of number using loop
+#include <stdio.h>
+    
+int main() {
+    int x = 6, v=1;
+    for(int i=1; i<=x; i++){
+        v = v*i;}
+    printf("%d\n", v);
+    return 87;
+}
