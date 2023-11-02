e3d88c24ab8e0e616567489ec38aec9623abe712
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-11-02 10:20:32 +0530

    Update nlp.py

--- py/nlp.py
+++ py/nlp.py
@@ -1,12 +1,7 @@
 import numpy as np
-sents = [
-    "Elephants are the largest living land mammals.",
-    "India is the world's largest democracy.",
-    "Engineers are destroying jobs, they even destroying their own jobs.",
-    "The world's largest desert is the Sahara, which covers nearly all of northern Africa.",
-    "India has largest popoulation in the world.",
-    "westbengal state has more population than canada.",
-]
+
+sents2 = """Elephants are the largest living land mammals. India is the world's largest democracy. Engineers are destroying jobs, they even destroying their own jobs. The world's largest desert is the Sahara, which covers nearly all of northern Africa.India has largest popoulation in the world.Westbengal state has more population than canada."""
+
 
 def filter(q:str) -> str:
     q = q.lower()
@@ -20,9 +15,22 @@ def filter(q:str) -> str:
     q = q.strip()
     return q
 
-q = "which  state has more popoulation then canada"
+
+def p2l(q:str):
+    q = q.lower()
+    q = q.replace("?", "")
+    q = q.replace(",", "")
+    q = q.replace("'", "")
+    q = q.replace('"', "")
+    q = q.replace("!", "")
+    q = q.replace("  ", " ")
+    q = q.replace("\n", "")
+    st=q.split(".")
+    q = q.strip()
+    return st
 
 def query(q:str, sents) -> str:
+    sents = p2l(sents)
     q = filter(q)
     matched = []
     x = np.array(q.lower().split())
@@ -33,11 +41,14 @@ def query(q:str, sents) -> str:
             perc = len(ints) / len(q.split())
             print(perc)
             matched.append({"m":len(ints), "a":s, "perc":perc})
-    if len(matched) == 0:
-        print("Sorry! but i don't have any details about it.")
-        return
+    if len(matched) == 0:return "I don't have enough information to answer that."
     matched = sorted(matched, key=lambda k: k['m'], reverse=True)
-    return matched[0]['a']
-    
+    return matched
 
-print(query(q))
+
+inp = input("Enter your the paragraph: ")
+while True:
+    inp = input("Enter your the paragraph: ")
+    if inp == "exit":
+        break
+    print(query(inp, sents2)[0]['a'])
