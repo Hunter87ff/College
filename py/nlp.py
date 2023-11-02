56b9cba37574a091987ef5cec4e90186908683d1
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-11-02 10:50:32 +0530

    Update nlp.py

--- py/nlp.py
+++ py/nlp.py
@@ -2,53 +2,37 @@ import numpy as np
 
 sents2 = """Elephants are the largest living land mammals. India is the world's largest democracy. Engineers are destroying jobs, they even destroying their own jobs. The world's largest desert is the Sahara, which covers nearly all of northern Africa.India has largest popoulation in the world.Westbengal state has more population than canada."""
 
+def token(q: str) -> str:
+    q = q.lower().translate(str.maketrans('', '', '^`~@?,\'"!;:')).replace("  ", " ").replace("\n", "").strip()
+    return q.split()
 
-def filter(q:str) -> str:
-    q = q.lower()
-    q = q.replace("?", "")
-    q = q.replace(".", "")
-    q = q.replace(",", "")
-    q = q.replace("'", "")
-    q = q.replace('"', "")
-    q = q.replace("!", "")
-    q = q.replace("  ", " ")
-    q = q.strip()
-    return q
-
-
-def p2l(q:str):
-    q = q.lower()
-    q = q.replace("?", "")
-    q = q.replace(",", "")
-    q = q.replace("'", "")
-    q = q.replace('"', "")
-    q = q.replace("!", "")
-    q = q.replace("  ", " ")
-    q = q.replace("\n", "")
-    st=q.split(".")
-    q = q.strip()
+def p2l(para:str):
+    para = para.lower().translate(str.maketrans('', '', '^`~@?\'"!;:')).replace("  ", " ").replace("\n", "").strip()
+    st=para.split(".")
     return st
 
-def query(q:str, sents) -> str:
-    sents = p2l(sents)
-    q = filter(q)
+def query(q:str, para) -> str:
+    sents = p2l(para)
+    q = token(q)
     matched = []
-    x = np.array(q.lower().split())
+    x = np.array(q)
     for s in sents:
         y = np.array(s.lower().split())
         ints = np.intersect1d(x, y)
         if len(ints) >0:
-            perc = len(ints) / len(q.split())
+            perc = len(ints) / len(q)
             print(perc)
-            matched.append({"m":len(ints), "a":s, "perc":perc})
+            matched.append({"m":len(ints), "a":s.strip(), "perc":perc})
     if len(matched) == 0:return "I don't have enough information to answer that."
     matched = sorted(matched, key=lambda k: k['m'], reverse=True)
     return matched
 
 
-inp = input("Enter the paragraph: ")
+pra = input("Enter the paragraph: ")
 while True:
-    inp = input("Enter your question: ")
-    if inp == "exit":
+    qry = input("Enter your question: ")
+    if qry == "exit":
         break
-    print(query(inp, sents2)[0]['a'])
+    print(query(qry, pra)[0]['a'])
+# q  = "which is the largest desert in the world?"
+# print(query(q, sents2)[0]['a'])
