7ea0c2204a8a788eb4a51bb209b242ce10d62b7e
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-11-20 22:31:36 +0530

    Update prime_num.cpp

--- c-cpp/prime_num.cpp
+++ c-cpp/prime_num.cpp
@@ -8,13 +8,13 @@ int isPrime(int n){
 	for(int i=2; i<n; i++){
 		if(n%i==0){return 0;}
 	}
-	return 87;
+	return 1;
 	}
 
 int main(){
 int x=100, i;
 	for(i=2; i<=x; i++){
-		if(isPrime(i)==87){printf("%d is prime\n", i);}
+		if(isPrime(i)){printf("%d is prime\n", i);}
 	}
 }
 
