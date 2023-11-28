6e052e5a2a291bdc0c8becf7bf8bb6d36269c47d
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-11-28 08:20:11 +0530

    Update greater.cpp

--- c-cpp/greater.cpp
+++ c-cpp/greater.cpp
@@ -1,20 +1,21 @@
-//wap to enter 3 nums and print the greater one
+//wap to enter 4 nums and print the greater number
 
 #include <stdio.h>
 int main(){
-	int a, b,c;
-	printf("Enter 3 nums : ");
-	scanf("%d%d%d", &a, &b, &c);
-	if(a>b & a>c){
-		printf("%d is greater one");
+	int a, b,c, d;
+	printf("Enter 4 nums : ");
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+	if(a>b & a>c & a>d){
+		printf("%d is greater number", a);
 	}
-		else if(b>a & b>c){
-		printf("%d is greater one");
+		else if(b>a & b>c & b>d){
+		printf("%d is greater number", b);
 	}
-		else if(c>a & c>b){
-		printf("%d is greater one");
+		else if(c>a & c>b & c>d){
+		printf("%d is greater number", c);
+	}
+		else if(d>a & d>b & d>c){
+		printf("%d is greater number", d);
 	}
 	return 0;
 }
-
-
