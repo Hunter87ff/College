de357d295d0282a91244a40472b75a34a357514d
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-12-02 15:41:33 +0530

    Update startup.bat

--- win/startup.bat
+++ win/startup.bat
@@ -1,14 +1,9 @@
 @echo off
 :a
-::-------V----Change this to your drive Letter
 if exist G:\ (goto Yes) else (goto a)
-
 :Yes
-::V----Change this to your drive Letter
 G:
-::----V----You can put any Program you want here
 start File.bat
 goto end
-
 :end
 exit
