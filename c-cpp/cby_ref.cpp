db5a4e0432064e086099c28ca7d1ad5da41e406d
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-12-05 10:03:56 +0530

    Update cby_ref.cpp

--- c-cpp/cby_ref.cpp
+++ c-cpp/cby_ref.cpp
@@ -1,13 +1,14 @@
+//call by reference function
 #include <stdio.h>
-void add(int *num);
+void add(int *num);  //declearation
 
 int main(){
     int x=20;
     printf("before %d\n", x);
-    add(&x);
+    add(&x);  //func called
     printf("after %d", x);
     return 0;
 }
-void add(int *num){
+void add(int *num){   //func defination
     (*num)++;
 }
