730888d977e8778392da5b101864cb6b8495f9b6
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-12-15 19:43:32 +0530

    Update pattern_np1_star.cpp

--- c-cpp/pattern_np1_star.cpp
+++ c-cpp/pattern_np1_star.cpp
@@ -2,17 +2,15 @@
 *
 **
 ***
+****
+*****
 */
 #include <stdio.h>
 int main() {
   int i, j, a,
   n = 5;
-  for(i = 0; i <= n; i++) {
-    a = 1;
-    for(j = 1; j <= i; j++) {
-      printf("*");
-      a++;
-    }
+  for(i = 1; i <= n; i++) {
+    for(j = 1; j <= i; j++) {printf("*");}
     printf("\n");
   }
   return 0;
