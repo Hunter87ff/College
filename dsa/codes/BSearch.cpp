a47b2e200b227648819dcce8e323d6d3bf67d393
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-03-02 13:36:21 +0530

    Update BSearch.cpp

--- dsa/codes/BSearch.cpp
+++ dsa/codes/BSearch.cpp
@@ -3,26 +3,33 @@
 int bSearch(int arr[], int l, int h, int target) {
     if (l <= h) {
         int m = l + (h - l) / 2;
-       // printf("%d", )
-        if (arr[m] == target)
-            return m;
-        if (arr[m] > target)
-            return bSearch(arr, l, m - 1, target);
-        return bSearch(arr, m + 1, h, target);
+        if (arr[m] == target){
+			return m;
+		}
+        else if (arr[m] > target){
+			return bSearch(arr, l, m - 1, target);
+		}
+        else{
+			return bSearch(arr, m + 1, h, target);
+		}
     }
     return -1; 
 }
-
+ 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};//demo array
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int target = 6;
+    int n;
+	printf("Enter the size:");
+	scanf("%d", &n);
+	int arr[n];
+	for(int i=0; i<n; i++){
+		printf("Enter number :");
+		scanf("%d", &arr[i]);
+	}
+    int target;
+    printf("enter the target : ");
+    scanf("%d", &target);
     int result = bSearch(arr, 0, n - 1, target);
-    if (result != -1){
-        printf("Element found at index %d.\n", result);
-    }
-    else{
-        printf("Element not found in the array.\n");
-    }
+    if (result != -1){printf("Element found at index %d.\n", result);}
+    else{printf("Element not found in the array.\n");}
     return 0;
 }
