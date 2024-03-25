cc2fa3fa35aadfe6bde6657fcc6ff355690419ad
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-03-25 18:09:18 +0530

    Update mc_server.py

--- py/mc_server.py
+++ py/mc_server.py
@@ -1,8 +1,11 @@
-import os
+import os, time
 from python_aternos import Client
 atclient = Client()
 atclient.login_with_session(os.environ["mc_sec"])
 aternos = atclient.account
 servs = aternos.list_servers()
 # myserv = servs[3]
-for i in servs:i.start()
+while True:
+  try:for i in servs:i.start()
+  time.sleep(10*60)
+  except:pass
