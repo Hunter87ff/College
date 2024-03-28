0554650566d701530a503039bea1d64c028a9547
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-03-29 00:01:56 +0530

    Create audio_scraper.py

---
+++ py/audio_scraper.py
@@ -0,0 +1,16 @@
+import json, os
+import requests as req
+songs = []
+for i in range(1, 9001):
+	url = f"https://pagalnew.com/download128/{i}"
+	headers = req.get(url).headers
+	print(f"{i}: ", headers["Content-Disposition"].split('"')[1])
+	data = {"filename":headers["Content-Disposition"].split('"')[1], "url":url}
+	songs.append(data)
+
+with open("songs.json", 'w') as json_file:
+    json.dump(songs, json_file, indent=4)
+
+webhook = os.environ["AUDIO_HOOK"]
+data = {"content": ""}
+req.post(webhook, json=data, files={"file": open("songs.json", "r")})
