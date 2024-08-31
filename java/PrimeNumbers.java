fa44a4021755795c3ec7d63b239ed14fe6da5afa
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-08-31 11:48:30 +0530

    Create PrimeNumbers.java
    Former-commit-id: 0946dc76553068f0c7a25ea7e59c0048bda5c7a9
    Former-commit-id: 7cdecea0ae314f16bf89fc381cb00f3feaace4c8

---
+++ java/PrimeNumbers.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Prime{
+	int i;
+	int primeCount=0;
+	int primeSum = 0;
+	Prime(int n){
+		countPrimes(n);
+	}
+
+	void print(String x){
+    System.out.print(x);
+	}
+
+	int isPrime(int k){
+		for(i=2; i<=(k/2);i++){
+			if(k%i==0){return 0;}
+		}
+		return 1;
+	}
+	
+	void countPrimes(int x) {
+		int count = 0;
+		int i = 2; 
+		while (i < x) {
+			if (isPrime(i) == 1) {
+				this.primeCount += 1;
+				this.primeSum += i;
+				print("\nPrime : " + i);
+			}
+			i++;
+		}
+	}
+}
+
+public class PrimeNumber{
+	
+	public static void main(String[] x){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int n = sc.nextInt();
+		Prime pc = new Prime(n);
+		System.out.print("\nPrime numebr count : " + pc.primeCount + "\nPrime number sum : "+pc.primeSum);
+	}
+}
