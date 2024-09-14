ef600a8ff4696575d6c97a42bb896156e7b45964
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-09-14 11:00:22 +0530

    Create GradeSystem.java
    Former-commit-id: 4cd78e614fc6ee9dbd568f1325ffb9d37639fc20
    Former-commit-id: 2bf9243b9e46330f9dbe1c5037a893b55b5e44ee

---
+++ java/GradeSystem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Grader{
+	String grade;
+	int score;
+	Grader(int score){this.score=score;}
+	
+	String letterGrade(){
+		if(score>100 || score <0){this.grade="Invalid";}
+		else if(this.score>=90){this.grade="O";}
+		else if(this.score>=80 && this.score<90){this.grade="E";}
+		else if(this.score>=70 && this.score<80){this.grade="A";}
+		else if(this.score>=60 && this.score<70){this.grade="B";}
+		else if(this.score>=50 && this.score<60){this.grade="C";}
+		else if(this.score<50){this.grade="F";}
+		return this.grade;
+	}
+}
+
+class GradeSystem{
+	public static void main(String[] x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your score : ");
+		Grader user_grade = new Grader(sc.nextInt());
+		System.out.println("You Grade : "+user_grade.letterGrade());
+	}
+}
