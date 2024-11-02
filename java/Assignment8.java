34bfe7096b1a573eeac19f61125aeb69fba90f55
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-11-02 09:42:48 +0530

    Update Assignment8.java
    Former-commit-id: a1bc1ecc4eaf2ebb9a2cac75bc34a306e5693418 [formerly d20c8a12dde6858b5b80c4e57f74c5467810be35]
    Former-commit-id: 12c6f915a2616bd87804958c80e4d9cf2b61ad2f
    Former-commit-id: 790e55f66c24f205512fef3e407965147b26c2da

--- java/Assignment8.java
+++ java/Assignment8.java
@@ -1,6 +1,7 @@
-class Assignment8{
+public class Assignment8{
     public static void main(String[] x){
-        int largest = 0, num;
+        int largest = Integer.MIN_VALUE, num;
+        if(x.length==0){System.out.println("\033[1;31m" + "Error: No input provided");System.out.print("\033[0m");return;}
         try {
             for (String x1 : x) {
                 num = Integer.parseInt(x1);
