d9259ce81b6cc8d923f6a13e0d9507ed95ddea28
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-11-09 09:39:51 +0530

    Create Assignment11.java
    Former-commit-id: 2f6e2adbea14c6e4b5df0ec11fce60440457b7e1 [formerly 8f8a35f9c0ac7dada7fdc1e6ca0f9aeddd55862b]
    Former-commit-id: d376c9b4da90383a7c31db7ae457c6d7d1078af2
    Former-commit-id: d029b61faff47332d791c19f8b9ded08580a78ab

---
+++ java/Assignment11.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class Assignment11 {
+    public static void main(String[] x) {
+        Vector<String> names = new Vector<>();
+        names.add("hunter87");
+        names.add("Neha");
+        names.add("sourav");
+        names.add("Akash");
+        for (String i : names) {
+            System.out.println(i);
+        }
+    }
+}
