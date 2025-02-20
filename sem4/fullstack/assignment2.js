bfdf1e37e292cf0171176a404183f0a0bc931606
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2025-02-20 17:40:16 +0530

    Create assignment2.js

---
+++ sem4/fullstack/assignment2.js
@@ -0,0 +1,33 @@
+// create a nodejs program to take user input and add two numbers and display the output
+
+
+
+import readline from "node:readline"
+
+
+
+const x = readline.createInterface(
+
+    process.stdin, process.stdout
+
+)
+
+x.question("Enter two number with space: ", (_a)=>{
+
+    let nums = _a.split(" ")
+
+    console.log("Addition : ", parseInt(nums[0]) + parseInt(nums[1]))
+
+})
+
+
+
+
+
+// create a node js program to display system env variables.
+
+
+
+
+
+console.log("System Environments : ", process.env)
