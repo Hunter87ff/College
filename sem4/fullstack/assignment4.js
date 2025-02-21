fd8e0397d02e96cd52fc8e300127a9f533553cda
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2025-02-21 22:42:25 +0530

    Update assignment4.js

--- sem4/fullstack/assignment4.js
+++ sem4/fullstack/assignment4.js
@@ -1,36 +1,21 @@
 // write a nodejs script for the REPL environment, including functions, array methods, and async operations.
 
-
-
+//assignment4.js
 function callMe(){
-
     console.log("87 is Enough");
-
 }
 
 const arr = [8,7,6,5]
-
 arr.push(9)
-
 console.log(arr);
 
-
-
 console.log("before timeout");
-
 setTimeout(()=>{console.log("Timeout")}, 3000);
-
 console.log("After Time out");
-
 callMe();
+//assignment4.js ends
 
-
-
-
-
-
-
+//terminal
 // $node
-
 // > .load ./assignment4.js
 
