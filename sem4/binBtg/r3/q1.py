b1a7119b3363b43d7515d6dc527682f59565187a
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2025-02-28 10:44:52 +0530

    Update q1.py
    Former-commit-id: 54e3ba7aaea8a7c382696ed206e0e1110d992c05 [formerly 0ab23cd204343d6c1fb4fb33dd7adb37684b50dc]
    Former-commit-id: f0f6304560d413c01add6ac87937873e89e5b518
    Former-commit-id: b93c25572882f0a6cc79d9bd5c8f278426ea148a

--- sem4/binBtg/r3/q1.py
+++ sem4/binBtg/r3/q1.py
@@ -1,77 +1,39 @@
-_arr = [
-
-    [1,3,1],
-
-    [1,5,1],
-
-    [6,2,4]
-
-]
-
-
 
 def isIndex(arr, i, j)-> int:
-
     try:
-
         return arr[i][j]
-
     except:
-
         return None
-
-
 
 def nextMax(arr:list, i, j):
-
-
-
     ij2 = isIndex(arr, i,j+1)
-
     i2j = isIndex(arr, i+1, j)
-
     if  ij2 and i2j:
-
         mx = max(ij2, i2j)
-
         return [ij2, i, j+1] if mx==ij2 else [i2j, i+1, j]
-
     elif ij2:
-
         return [ij2, i, j+1]
-
     elif i2j:
-
         return [i2j, i+1, j]
-
     else:
-
         return None
-
 
 
 def sumMax(arr, m=3, n=3):
-
     sum=0
-
     i,j=0,0
-
     for _ in range(m+1):
-
         nm = nextMax(arr, i,j)
-
         if nm:
-
             i,j = nm[1:3]
-
             print(nm[0], end=" ")
-
             sum+=nm[0]
-
     print()
-
     print(sum)
 
-
-
+_arr = [
+    [1,3,1],
+    [1,5,1],
+    [6,2,4]
+]
 sumMax(_arr, 3,3)
