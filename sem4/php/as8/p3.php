af5a24d41257b6496cc021d4c1c3d3551194a2ba
Hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2025-06-21 20:11:54 +0530

    Update p3.php

--- sem4/php/as8/p3.php
+++ sem4/php/as8/p3.php
@@ -1,22 +1,5 @@
 <!-- Write a division table program in PHP using for loop. -->
-<!DOCTYPE html>
-<style>
-    table, th, td {border: 1px solid black;}
-    th {background: #f2f2f2;}
-</style>
-
-<h2>Division Table</h2>
 <?php
-$size = 10; 
-echo "<table>";
-echo "<tr><th></th>";
-for($i = 1; $i <= $size; $i++) {echo "<th>$i</th>";}
-echo "</tr>";
-for($i = 1; $i <= $size; $i++) {
-    echo "<tr> <th>$i</th>";
-    for($j = 1; $j <= $size; $j++) {
-        $result = round($i / $j, 3);
-        echo "<td>$result</td>";
-    } echo "</tr>";
-} echo "</table>";
+$n = $_GET['n'] ?? 5;
+for ($i = 1; $i <= 10; $i++) echo "$n / $i = " . round($n / $i, 2) . "<br>";
 ?>
